cef241ad66d8cb605043576cae5e789f5882c175
hieunt <hieu29072004@gmail.com>
2026-04-11 21:01:33 +0700

    commit

--- src/main/java/hieunt/stock/spark/processor/BenchmarkIndexProcessor.java
+++ src/main/java/hieunt/stock/spark/processor/BenchmarkIndexProcessor.java
@@ -5,14 +5,15 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
-public class BenchmarkIndexProcessor extends AbstractMinioToPostgresProcessor {
+public class BenchmarkIndexProcessor extends AbstractMinioToClickhouseProcessor {
     public BenchmarkIndexProcessor(String partitionPath) {
-        super("benchmark_index", "stock.benchmark_index", "benchmark_index", partitionPath);
+        super("benchmark_index", "stock.fact_benchmark_index", "benchmark_index", partitionPath);
     }
 
     @Override
     protected Dataset<Row> transform(Dataset<Row> df) {
         return df.withColumn("time", functions.to_timestamp(functions.col("time")))
+                .withColumn("date_id", functions.to_date(functions.col("time")))
                 .withColumn("open", functions.col("open").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("high", functions.col("high").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("low", functions.col("low").cast(DataTypes.createDecimalType(20, 2)))

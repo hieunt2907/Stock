1df76a8448fab1dc5266157f386898a68772176b
hieunt <hieu29072004@gmail.com>
2026-04-12 19:23:04 +0700

    commit

--- src/main/java/hieunt/stock/spark/processor/DailyPriceProcessor.java
+++ src/main/java/hieunt/stock/spark/processor/DailyPriceProcessor.java
@@ -5,14 +5,15 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
-public class DailyPriceProcessor extends AbstractMinioToPostgresProcessor {
+public class DailyPriceProcessor extends AbstractMinioToClickhouseProcessor {
     public DailyPriceProcessor(String partitionPath) {
-        super("daily_price", "stock.daily_price", "daily_price", partitionPath);
+        super("daily_price", "stock.fact_daily_price", "daily_price", partitionPath);
     }
 
     @Override
     protected Dataset<Row> transform(Dataset<Row> df) {
         return df.withColumn("time", functions.to_timestamp(functions.col("time")))
+                .withColumn("date_id", functions.to_date(functions.col("time")))
                 .withColumn("open", functions.col("open").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("high", functions.col("high").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("low", functions.col("low").cast(DataTypes.createDecimalType(20, 2)))

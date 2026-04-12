67394bcf42a7e303cf5564c9472bdd8678a509a8
hieunt <hieu29072004@gmail.com>
2026-04-12 19:22:58 +0700

    commit

--- src/main/java/hieunt/stock/spark/processor/DailyPriceBackfillProcessor.java
+++ src/main/java/hieunt/stock/spark/processor/DailyPriceBackfillProcessor.java
@@ -5,14 +5,15 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
-public class DailyPriceBackfillProcessor extends AbstractMinioToPostgresProcessor {
+public class DailyPriceBackfillProcessor extends AbstractMinioToClickhouseProcessor {
     public DailyPriceBackfillProcessor(String partitionPath) {
-        super("daily_price_backfill", "stock.daily_price_backfill", "daily_price_backfill", partitionPath);
+        super("daily_price_backfill", "stock.fact_daily_price", "daily_price_backfill", partitionPath);
     }
 
     @Override
     protected Dataset<Row> transform(Dataset<Row> df) {
         return df.withColumn("time", functions.to_timestamp(functions.col("time")))
+                .withColumn("date_id", functions.to_date(functions.col("time")))
                 .withColumn("open", functions.col("open").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("high", functions.col("high").cast(DataTypes.createDecimalType(20, 2)))
                 .withColumn("low", functions.col("low").cast(DataTypes.createDecimalType(20, 2)))
